e5ddf814023db643a765e7656e78890a2143be4a
VaibhaviZamde <2023bit503@sggs.ac.in>
2024-01-29 14:47:26 +0530

    Simple array program which print array element and message which have public class

---
+++ Array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Array
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		int arr[] = {10 ,20,30};
+		int size = arr.length;
+		
+		System.out.println("Array elements are : ");
+		for(int i = 0;i<size;i++)
+		{
+			System.out.println(" "+arr[i]);
+		}
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
